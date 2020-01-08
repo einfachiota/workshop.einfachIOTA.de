9cb54edf034d38780bb75e83d100b117bc20775d
Sebastian Heusser <huhn@Sebastians-MBP.fritz.box>
2020-01-08 06:35:21 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# iota-js-workshop-slides
+
+> Slides for the IOTA javascript workshop
+
+## Build Step
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run lint
+npm run lint
+```
---
+++ src/Home.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="frontpage">
+    <div class="content">
+      <h1>
+        <span class="logo"></span>
+        <span>Workshop</span>
+      </h1>
+      <h2>Lerne die Grundlagen von IOTA und wie man mit dem Tangle arbeitet.</h2>
+      <iframe
+        class="github-star"
+        src="https://ghbtns.com/github-btn.html?user=einfachiota&amp;repo=einfachIOTA.de&amp;type=star&amp;count=true&amp;size=large"
+        frameborder="0"
+        scrolling="0"
+        width="160px"
+        height="30px"
+      ></iframe>
+      <h3>IOTA Grundlagen</h3>
+      <div class="thumbnails">
+        <div class="box-card" v-for="(slideshow, index) in introduction" :key="index">
+          <router-link :to="slideshow.infos.path" @click.native="click">
+            <div class="embedded-slideshow-container">
+              <component
+                :is="slideshow"
+                :embedded="true"
+                :keyboardNavigation="false"
+                :mouseNavigation="false"
+              ></component>
+            </div>
+          </router-link>
+          <div class="caption">
+            <h3>{{slideshow.infos.title}}</h3>
+            <p class="thumbnail-description">{{slideshow.infos.description}}</p>
+          </div>
+        </div>
+      </div>
+      <h3>Javascript IOTA Workshop</h3>
+      <p>
+        Der Javascript IOTA Workshop hilft Entwicklern, und jene, die es werden wollen, bei dem Einstieg in den Tangle.
+      </p>
+      <p>Mit einer Schritt für Schritt anleitung und viele nützliche Tipps ist dieser Workshop der Perfekt <Startpunkt class="s"></Startpunkt></p>
+      <div class="thumbnails">
+        <div class="box-card" v-for="(slideshow, index) in slideshows" :key="index">
+          <router-link :to="slideshow.infos.path" @click.native="click">
+            <div class="embedded-slideshow-container">
+              <component
+                :is="slideshow"
+                :embedded="true"
+                :keyboardNavigation="false"
+                :mouseNavigation="false"
+              ></component>
+            </div>
+          </router-link>
+          <div class="caption">
+            <h3>{{slideshow.infos.title}}</h3>
+            <p class="thumbnail-description">{{slideshow.infos.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import slideshows from 'slideshows/slideshows'
+
+export default {
+  data: function () {
+    return {
+      introduction: slideshows.introduction,
+      slideshows: slideshows.list
+    }
+  },
+  mounted: function () {
+    console.log(this.slideshows)
+    document.currentSlide = {}
+  },
+  methods: {
+    click: function (evt) {
+      evt.stopPropagation()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+@import "node_modules/eagle.js/src/themes/frontpage/frontpage";
+
+.logo {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  margin-right: 0.1em;
+  background-image: url(./logo.svg);
+  background-size: contain;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+}
+
+.github-star {
+  display: block;
+  margin: 0 auto;
+  margin-top: -10px;
+}
+</style>
